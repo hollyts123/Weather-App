d4e027da1b449e0b0466d50bc8208e273e2dfcf9
Tatiana Slonovskaya <holly@i.ua>
2022-07-02 14:00:25 +0300

    Added real data for a default city and for the requested city

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// Feature #1
+// Displaying the current date
 let currentDate = new Date();
 let currentHours = currentDate.getHours();
 let currentMinutes = currentDate.getMinutes();
@@ -20,7 +20,25 @@ let currentDay = days[currentDate.getDay()];
 let p = document.querySelector(".text p");
 p.innerHTML = `${currentDay} ${currentHours}:${currentMinutes}`;
 
-// Feature #2
+// Displaying the weather for a default city
+function displayDefaultTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = `${Math.round(response.data.main.temp)}°C`;
+  let description = document.querySelector("#description");
+  description.innerHTML = `${response.data.weather[0].description}`;
+  let humidity = document.querySelector("#humidity");
+  humidity.innerHTML = `Humidity: ${response.data.main.humidity}%`;
+  let wind = document.querySelector("#wind");
+  wind.innerHTML = `Wind: ${Math.round(response.data.wind.speed)} Km/H`;
+  
+  console.log(defaultCityApiUrl);
+}
+
+let apiKey = "b533bb31ab6b7d26400f4e2f73516e81";
+let defaultCityApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Nikopol&appid=${apiKey}&units=metric`;
+axios.get(defaultCityApiUrl).then(displayDefaultTemperature);
+
+// Searching for a city and displaying the weather
 
 function showWeather(response) {
   let city = document.querySelector(".city");
@@ -28,8 +46,14 @@ function showWeather(response) {
   city.innerHTML = `${searchInput.value}`;
 
   let temp = response.data.main.temp;
-  let temperature = document.querySelector(".degree");
+  let temperature = document.querySelector("#temperature");
   temperature.innerHTML = `${Math.round(temp)}°C`;
+  let weatherDescription = document.querySelector("#description");
+  weatherDescription.innerHTML = `${response.data.weather[0].description}`;
+  let humidity = document.querySelector("#humidity");
+  humidity.innerHTML = `Humidity: ${response.data.main.humidity}%`;
+  let wind = document.querySelector("#wind");
+  wind.innerHTML = `Wind: ${Math.round(response.data.wind.speed)} Km/H`;
 }
 
 function searchCity(event) {
@@ -44,19 +68,19 @@ function searchCity(event) {
 let search = document.querySelector(".search-bar");
 search.addEventListener("submit", searchCity);
 
-// Bonus Feature
-// function convertToCelsius(event) {
-//   event.preventDefault();
-//   let degree = document.querySelector(".degree");
-//   degree.innerHTML = "15°C";
-// }
-// function convertToFahrenheit(event) {
-//   event.preventDefault();
-//   let degree = document.querySelector(".degree");
-//   degree.innerHTML = "59°F";
-// }
-// let fahrenheit = document.querySelector("#fahrenheit");
-// fahrenheit.addEventListener("click", convertToFahrenheit);
+// Converting temperature
+function convertToCelsius(event) {
+  event.preventDefault();
+  let degree = document.querySelector(".degree");
+  degree.innerHTML = "";
+}
+function convertToFahrenheit(event) {
+  event.preventDefault();
+  let degree = document.querySelector(".degree");
+  degree.innerHTML = "";
+}
+let fahrenheit = document.querySelector("#fahrenheit");
+fahrenheit.addEventListener("click", convertToFahrenheit);
 
-// let celsius = document.querySelector("#celsius");
-// celsius.addEventListener("click", convertToCelsius);
+let celsius = document.querySelector("#celsius");
+celsius.addEventListener("click", convertToCelsius);
